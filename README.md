debcb2e922b38d79de007cd26481c6976eae3dff
ndadri <adrianfalcones08@gmail.com>
2026-05-06 08:53:28 -0600

    Conexion a la base de datos

---
+++ README.md
@@ -0,0 +1,12 @@
+# pasos para iniciar 
+
+- python app.py
+
+# librerias instaladas
+
+- python -m pip install flask-sqlalchemy
+- python -m pip install python-dotenv
+
+# puerto de la bdd
+
+- 1433
--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# 1. CARGAMOS LAS VARIABLES DE ENTORNO DESDE .env (PRIMERA LINEA DE PROTECCION)
+
+load_dotenv()
 
 app = Flask(__name__)
 
+# 2. CONSTRUCCION DIDACTICA DE LA CONEXION CON POSTRES
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+# LA RECETA DE LA URI PARA CONECTARSE
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# 3. INSTANCIA DE SQLALCHEMY (LA LLAVE MAESTRA)
+
+db = SQLAlchemy(app)
+
 @app.route("/")
-def hello_world():
-    return "<p>¡Hola! Flask funcionando en mi Mac Silicon.</p>"
+def test_connection():
+    return jsonify({
+        "status": "online",
+        "message": "Configuracion de base de datos cargada correctamente",
+        "user_connected": DB_USER
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
